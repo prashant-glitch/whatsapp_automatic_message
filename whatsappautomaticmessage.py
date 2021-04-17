e2b3caab63a6680a9a90e2d90c0fa8e370f29d44
prashant kumar <76524080+prashant-glitch@users.noreply.github.com>
2021-04-17 11:11:18 +0530

    Update whatsappautomaticmessage.py

--- whatsappautomaticmessage.py
+++ whatsappautomaticmessage.py
@@ -1,4 +1,4 @@
 import pywhatkit
 #pywhatkit.sendwhatmsg('+country code phone number','message you want to deliver',time_in_hour,minute)
-pywhatkit.sendwhatmsg('+91**********','Hey bro how are you what are you doing right now , can we meet int the evening',11 ,10)
+pywhatkit.sendwhatmsg('+91**********','Hey bro how are you what are you doing right now , can we meet in the evening',11 ,10)
 
